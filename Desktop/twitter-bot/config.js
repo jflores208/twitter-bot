969373549ca36799f998bea268e8501c42c00548
Jasmine Flores <jflores208@cnm.edu>
2021-03-01 19:57:47 -0700

    Committing to git repo

---
+++ Desktop/twitter-bot/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    consumer_key: 'WfDA436tceJItjq6cZXTOYeFv',
+    consumer_secret: 'IJ1jfAP3MeQJninShhOjUhJ9x98BWMRLn1AxQ0BDnYLEXhgos1',
+    access_token_key: '1366549526165684226-ZipDttl6diBCZsjsGJ0YBxZKfOpgdD',
+    access_token_secret: 'FC185KqGCQ6YsKLu1v2aw9yNgyjIM1LVm9svUEP9ykCSc'
+  }
+
+  
